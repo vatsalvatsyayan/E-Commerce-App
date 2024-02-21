9fdcf41853792cc9a2d87765807c6759d02dc3ab
Vatsal Vatsyayan <vatsaljackspicer@gmail.com>
2024-02-21 17:49:53 +0530

    Added Frontend and CORS Support in backend

--- Backend/spring-boot-ecommerce/src/main/java/com/Vatsal/ecommerce/dao/ProductRepository.java
+++ Backend/spring-boot-ecommerce/src/main/java/com/Vatsal/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.Vatsal.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.Vatsal.ecommerce.entity.Product;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
     
 }
