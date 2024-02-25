072072b386ac27c2c4ee3d8f8bcc1f255ab28865
Vatsal Vatsyayan <vatsaljackspicer@gmail.com>
2024-02-25 18:21:20 +0530

    Added Search Component

--- Frontend/angular-ecommerce/src/app/component/product-list/product-list.component.ts
+++ Frontend/angular-ecommerce/src/app/component/product-list/product-list.component.ts
@@ -15,6 +15,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  searchMode: boolean = false;
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
@@ -23,6 +24,32 @@ export class ProductListComponent implements OnInit {
   }
   
   listProducts() {
+    
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    
+    if (this.searchMode)
+    {
+      this.handleSearchProducts();
+    }
+    else
+    {
+      this.handleListProducts();
+    }
+
+  }
+
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    this.productService.searchProducts(theKeyword).subscribe(
+        data => {
+          this.products = data;
+        }
+    );
+  }
+
+  handleListProducts() 
+  {
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     
     if(hasCategoryId)
--- Frontend/angular-ecommerce/src/app/services/product.service.ts
+++ Frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -9,7 +9,7 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
-
+  
   private baseUrl = 'http://localhost:8080/api/products';
 
   private categoryUrl = 'http://localhost:8080/api/product-category';
@@ -20,6 +20,16 @@ export class ProductService {
     
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
     
+    return this.getProducts(searchUrl);
+  }
+
+  searchProducts(theKeyword: string) : Observable<Product[]>{
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+    
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
